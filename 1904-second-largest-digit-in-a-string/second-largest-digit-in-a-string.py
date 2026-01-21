fdf2f8b3effa4d84123ac7fbddfe825d78a37847
Nida Noushad <139417240+NidaNoushad@users.noreply.github.com>
2026-01-22 00:46:05 +0530

    Time: 3 ms (81.60%) | Memory: 19.5 MB (9.14%) - LeetSync

---
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        digits=set()
+        for ch in s:
+            if ch.isdigit():
+                digits.add(int(ch))
+            
+
+        if len(digits)<2:
+            return -1
+        
+        digits=sorted(digits)
+        return digits[-2]
+        
